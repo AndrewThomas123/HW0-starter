5b6fa49c83ee4463e292d02a3d6baa8cf3b7b357
Matthew Hibbs <mhibbs@trinity.edu>
2018-01-25 21:15:13 -0600

    Initial commit

---
+++ src/cs2/util/Complex.scala
@@ -0,0 +1,24 @@
+package cs2.util
+
+class Complex (private var r:Double, private var i:Double) {
+  def real():Double = { }
+	def imag():Double = { }
+	
+	def + (other:Complex):Complex = { }
+	def - (other:Complex):Complex = { }
+	
+	def * (other:Complex):Complex = { }
+	def / (other:Complex):Complex = { }
+	
+	def conjugate():Complex = { }	
+}
+	
+object hw0 {
+	/** Use this main function to create a tester for your Complex number class
+	 *  Be sure to test construction of Complex objects, calling each of the methods
+	 *  defined above, and the use of "corner case" values
+	 */
+	def main(args:Array[String]) {
+	
+	}
+}
